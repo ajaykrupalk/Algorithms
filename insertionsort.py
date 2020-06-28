2eb6c0b0d4e3fc2520951fa9b22d15852c1c2424
ajaykrupalk <67155124+ajaykrupalk@users.noreply.github.com>
2020-06-28 18:48:02 +0530

    Create insertionsort.py

---
+++ insertionsort.py
@@ -0,0 +1,17 @@
+'''Program to demonstrate Insertion Sort'''
+def inssort(array):
+  for i in range(len(array)):
+    j=i
+    while j>=1:
+      if array[j]<array[j-1]:#comparison of previous elements
+      #swapping of elements
+        temp=array[j]
+        array[j]=array[j-1]
+        array[j-1]=temp
+      j-=1#decreementing the index
+  print('The Sorted Array is:')
+  for i in array:
+    print(i,end=" ")
+  print()
+
+inssort([23,45,12,67,32,7])
